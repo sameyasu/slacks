f451645e779d6114fb05020cecdb7bcf4b886927
sameyasu <sameyasu@gmail.com>
2019-03-07 00:04:10 +0900

    Refactor config.rs

--- src/config.rs
+++ src/config.rs
@@ -3,20 +3,45 @@ use super::*;
 use std::fs::File;
 use std::io::{self, Write, BufReader, BufWriter};
 
-#[derive(Serialize,Deserialize,Debug)]
+#[derive(Serialize,Deserialize,Debug,Clone)]
 pub struct Configs {
     pub webhook_url: String,
     pub debug_mode: bool
 }
 
+impl Configs {
+
+    fn load(&mut self, path: &str) -> Result<(), String> {
+        File::open(path)
+            .map_err(|e| e.to_string())
+            .and_then(|file| {
+                let c : Configs = serde_json::de::from_reader(BufReader::new(file))
+                    .map_err(|e| e.to_string())
+                    .unwrap();
+
+                self.clone_from(&c);
+                Ok(())
+            })
+    }
+
+    fn save(&self, path: &str) -> Result<(), String> {
+        File::create(path)
+            .map_err(|e| e.to_string())
+            .and_then(|file|
+                serde_json::ser::to_writer_pretty(BufWriter::new(file), self)
+                    .map_err(|e| e.to_string())
+                    .and_then(|_| Ok(()))
+            )
+    }
+}
+
 pub fn configure(debug: bool) {
-    let configs = get_configs(debug)
-        .unwrap_or(
-            Configs {
-                webhook_url: "".to_string(),
-                debug_mode: debug
-            }
-        );
+    let mut configs = Configs {
+       webhook_url: "".to_string(),
+       debug_mode: debug
+    };
+
+    let _ = configs.load(&get_config_path());
 
     let current_url = configs.webhook_url.to_string();
     let mut webhook_url = "".to_string();
@@ -30,65 +55,42 @@ pub fn configure(debug: bool) {
         webhook_url: webhook_url,
         debug_mode: false // allways false
     };
-    save_config_file(&get_config_path(), &new_conf).unwrap();
+    new_conf.save(&get_config_path()).unwrap();
     println!("Config: {:?}", &new_conf);
 }
 
 pub fn get_configs(is_debug_mode: bool) -> Result<Configs, Error> {
-    match load_config_file(&get_config_path()) {
-        Ok(c) => Ok(
-            Configs {
-                webhook_url: match &c.webhook_url {
-                    url if url.is_empty() =>
-                        std::env::var("SLACK_WEBHOOK_URL").unwrap_or("".to_string()),
-                    url => url.to_string()
-                },
-                debug_mode: is_debug_mode
-            }
-        ),
-        Err(e) => {
+    let mut configs = Configs {
+       webhook_url: "".to_string(),
+       debug_mode: is_debug_mode
+    };
+
+    let _ = configs.load(&get_config_path())
+        .map_err(|e| {
             if is_debug_mode {
-                println!("Failed to load config file. Causes: {}", e);
+                println!("Failed to load config file. Cause: {}", e);
             }
-            match std::env::var("SLACK_WEBHOOK_URL") {
-                Ok(url) => Ok(
-                    Configs {
-                        webhook_url: url,
-                        debug_mode: is_debug_mode
-                    }
-                ),
-                Err(_) => Err(
-                    Error::Argv("SLACK_WEBHOOK_URL is not set.".to_string())
-                )
+        })
+        .map(|()| {
+            if is_debug_mode {
+                println!("{:?}", configs);
             }
-        }
-    }
+        });
+
+    let _ = std::env::var("SLACK_WEBHOOK_URL")
+        .map(|url| {
+            // override: backward compatibility
+            configs.webhook_url = url;
+        });
+
+    Ok(configs)
+
 }
 
 fn get_config_path() -> String {
     std::env::var("HOME")
         .map(|home| format!("{}{}", home, DEFAULT_CONFIG_PATH).to_string())
         .unwrap_or(DEFAULT_CONFIG_PATH.to_string())
-}
-
-fn load_config_file(path: &str) -> Result<Configs, String> {
-    File::open(path)
-        .map_err(|e| e.to_string())
-        .and_then(|file|
-            serde_json::de::from_reader(BufReader::new(file))
-                .map_err(|e| e.to_string())
-                .and_then(|c| Ok(c))
-        )
-}
-
-fn save_config_file(path: &str, conf: &Configs) -> Result<(), String> {
-    File::create(path)
-        .map_err(|e| e.to_string())
-        .and_then(|file|
-            serde_json::ser::to_writer_pretty(BufWriter::new(file), conf)
-                .map_err(|e| e.to_string())
-                .and_then(|_| Ok(()))
-        )
 }
 
 fn read_line() -> Result<String, Error> {
